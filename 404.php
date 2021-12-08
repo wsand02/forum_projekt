b86b1659702a0ca377d81b728f1fb1faa8078923
William Sandbrink <wilsandbrink@gmail.com>
2021-12-08 10:55:06 +0100

    404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+require_once("common.php");
+
+$pageTitle = "Sidan kunde inte hittas";
+
+require_once("inc/header.php");
+?>
+<div class="not-found">
+  <h1>404</h1>
+  <p>Sidan du letar efter kunde inte hittas.</p>
+  <a href="index.php">Gå tillbaka</a>
+</div>
+<?php require_once("inc/footer.php"); ?>
